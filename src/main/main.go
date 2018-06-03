7a8d45bda46340131bb5d4f0fbeb1103c32fff6e
Floyd Kots <floydkotohoyoh@gmail.com>
2018-06-03 11:39:33 +0300

    Exercise templates and sub-templates

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,52 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+	"text/template"
+)
 
 func main() {
-	http.ListenAndServe(":8000", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Add("Content-Type", "text/html")
+		templates := template.New("template")
+		templates.New("test").Parse(doc)
+		templates.New("header").Parse(header)
+		templates.New("footer").Parse(footer)
+
+		context := Context{
+			[3]string{"Lemon", "Orange", "Apple"},
+			"the title",
+		}
+		templates.Lookup("test").Execute(w, context)
+	})
+
+	http.ListenAndServe(":8000", nil)
+}
+
+const header = `
+<!DOCTYPE html>
+<html>
+  <head><title>{{.}}</title></head>
+`
+
+const doc = `
+{{template "header" .Title}}
+  <body>
+    <h1>List of Fruits</h1>
+	<ul>
+		{{range .Fruit}}
+			<li>{{.}}</li>
+		{{end}}
+	</ul>
+  </body>
+{{template "footer"}}
+`
+
+const footer = `
+</html>
+`
+
+type Context struct {
+	Fruit [3]string
+	Title string
 }
